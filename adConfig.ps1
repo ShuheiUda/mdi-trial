dbb91a7873c07ed3cea7001c4a1805f6f20a860e
Shuhei Uda <shuda@microsoft.com>
2024-12-25 12:34:23 +0900

    test

--- adConfig.ps1
+++ adConfig.ps1
@@ -1,3 +1,6 @@
+#AD のインストール
+Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+
 # Create Tools Directory if it does not exist
 $toolsDir = "C:\Tools"
 if ((Test-Path -Path $toolsDir) -eq $false) {New-Item -Path C:\ -Name Tools -ItemType Directory}
@@ -48,36 +51,6 @@ Get-ChildItem -Recurse -Path $toolsDir | Unblock-File -Confirm:$false
 
 # Remove only .zip files from the specified folder
 Get-ChildItem -Path $toolsDir -Filter *.zip -Recurse | Remove-Item -Force
-
-#日本語の言語パックインストール
-Install-Language ja-JP -CopyToSettings
-
-#表示言語を日本語に変更する
-Set-systemPreferredUILanguage ja-JP
-
-#UIの言語を日本語で上書きします
-Set-WinUILanguageOverride -Language ja-JP
-
-#時刻/日付の形式をWindowsの言語と同じにします
-Set-WinCultureFromLanguageListOptOut -OptOut $False
-
-#タイムゾーンを東京にします
-Set-TimeZone -id "Tokyo Standard Time"
-
-#ロケーションを日本にします
-Set-WinHomeLocation -GeoId 0x7A
-
-#システムロケールを日本にします
-Set-WinSystemLocale -SystemLocale ja-JP
-
-#ユーザーが使用する言語を日本語にします
-Set-WinUserLanguageList -LanguageList ja-JP,en-US -Force
-
-#入力する言語を日本語で上書きします
-Set-WinDefaultInputMethodOverride -InputTip "0411:00000411"
-
-#ようこそ画面と新しいユーザーの設定をコピーします
-Copy-UserInternationalSettingsToSystem -welcomescreen $true -newuser $true
 
 #サーバーを再起動します
 Restart-Computer 
